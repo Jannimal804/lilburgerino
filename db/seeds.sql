f882dfe5bfa09f82bd95df59beabe6765da653a5
Jannimal804 <jannalhall@gmail.com>
2019-02-28 23:50:45 -0500

    updated 2/28 with more styling and functionality

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kangaroo Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Alligator Burger', FALSE);
